dc9626488b34b2f7a448ac498a46f7be92ede50d
Himanshu Singh <himanshu11sgh@gmail.com>
2022-08-31 08:40:07 +0530

    *31 Aug 2022

--- django/otps/projects/site_under_construction_project/site_under_construction_project/urls.py
+++ django/otps/projects/site_under_construction_project/site_under_construction_project/urls.py
@@ -7,5 +7,5 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
+    path('about/', views.about, name='about'), 
 ]
