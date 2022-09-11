18e091776c8371349f5e6753df8104bf4fd5d9fb
Himanshu Singh <himanshu11sgh@gmail.com>
2022-09-11 09:51:50 +0530

    Sep 11 - 2

--- jquery/projects/crud_jquery_project/core/views.py
+++ jquery/projects/crud_jquery_project/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.core import serializers
 from django.forms.models import model_to_dict
 import json
@@ -16,6 +16,7 @@ def home(request):
     
     if request.method == 'POST':
         print(request.POST)
+        print(type(request.POST))
         form = StudentForm(data=request.POST)
         id = request.POST.get('id')
         if form.is_valid():
@@ -31,11 +32,17 @@ def home(request):
 
             student.save()
 
-            d = model_to_dict(student)
+            # convert modal objects into json by using values and list method 
+            # students = Student.objects.values()
+            # students_list = list(students)
 
-            students = Student.objects.values()
-            students_list = list(students)
+            # convert model objects into json by using core.serializers method
+            students = Student.objects.all()
+            students_list = serializers.serialize("json", students)
+            # print(students_list)
+            # print(type(students_list))
 
+            # return HttpResponse(json.dumps({'status': '1', 'students': students_list}), content_type='application/json')
             return JsonResponse({'status': '1', 'students': students_list})
         else:
             return JsonResponse({'status': '0'})
@@ -56,6 +63,14 @@ def update(request):
         id = request.POST.get('id')
         try:
             student = Student.objects.get(id=id)
+
+            # convert model instance into json by using core.serializers method
+            student_dict = serializers.serialize("json", [student,])
+            # print(type(student_dict))
+            # print(type(json.dumps(student_dict)))
+
+
+            # convert model instance into json by using model_to_dict method 
             student_dict = model_to_dict(student)
             return JsonResponse({'status': '1', 'student': student_dict})
         except:
