9133d8ab678cad5d1704d1e4dab1ff90c1019aca
Himanshu Singh <himanshu11sgh@gmail.com>
2022-09-17 21:14:26 +0530

    Add: Bitcoin_wallet project

---
+++ django/otps/projects/bitcoin_wallet_project/core/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+
+from core.models import Student
+
+# Register your models here.
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "email", "password"]
---
+++ django/otps/projects/bitcoin_wallet_project/core/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, "core/index.html")
+
+
+def login(request):
+    return render(request, "core/login.html")
+
+
+def register(request):
+    return render(request, "core/register.html")
+
+
+
